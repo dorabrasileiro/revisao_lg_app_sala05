5b92e61370f591d3601bf3615e6843641fdcb4e4
dorabrasileiro <dora2020br@gmail.com>
2026-07-27 08:36:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function calcular(){
+    var nome = document.getElementById('nome').value 
+    var nota1 = parseFloat(document.getElementById('nota1').value)
+    var nota2 = parseFloat(document.getElementById('nota2').value)
+    var nota3 = parseFloat(document.getElementById('nota3').value)
+    //console.log(nome, nota1, nota2, nota3)
+    var media = ((nota1 + nota2 + nota3) / 3)
+    var res = media * 3
+    var print = `Seu nome é ${nome}, sua média foi ${media}, com a multiplicação o valor fica: ${res}.`
+    document.getElementById('resposta').textContent = print 
+}
